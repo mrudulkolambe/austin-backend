7a5198371cbc7ad6a7d0acef8a137a6fd9b87ca8
Prachi More <prachimore2702@gmail.com>
2023-05-21 17:55:58 +0530

    changes

--- Controllers/Teacher.js
+++ Controllers/Teacher.js
@@ -6,9 +6,10 @@ const createTeacher = async (req, res) => {
 		const salt = await bcrypt.genSalt(10)
 		const hashedPassword = await bcrypt.hash(req.body.password, salt)
 		const newTeacher = new Teacher({ ...req.body, password: hashedPassword });
-		const finalTeacher = (await newTeacher.save()).populate('subject');
+		const finalTeacher = await newTeacher.save();
+		const teacher = await Teacher.findById(finalTeacher._id).populate('subject')
 		if (finalTeacher) {
-			res.json({ error: false, message: 'Teacher created successfully!', teacher: finalTeacher })
+			res.json({ error: false, message: 'Teacher created successfully!', teacher: teacher })
 		} else {
 			res.json({ error: true, message: 'Something went wrong!', teacher: undefined })
 		}
